90436eda5bac298365363a6b58a7630850ee5c7f
neelakarthi98-rgb <neelakarthi98@gmail.com>
2025-11-02 06:33:03 +0530

    Miniproject-3(Temperature Converter)

---
+++ miniproject3.py
@@ -0,0 +1,8 @@
+c_tem=float(input("Enter the celsius: "))
+
+#convert celusius to Fahrenheit using arithmetic operation
+
+F_tem=float(c_tem*9/5)+32
+#print the values
+
+print(f"{c_tem}c is equal to {F_tem}f")
